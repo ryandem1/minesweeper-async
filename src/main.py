05dc77a1bf517d3d27ef2396e283b39a731638dc
ryandemarigny <ryandemarigny@gmail.com>
2022-11-05 20:24:43 -0700

    `/check` now checks if a board exists

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import models
 app = FastAPI()
 
 SCORE = 0
+OUTSTANDING_BOARDS: list[models.Board] = []  # Boards that have been requested but not returned
+BOARD_SIZE_LIMIT = 50  # Amount of boards that can be outstanding at once
 
 
 @app.get("/score")
@@ -11,17 +13,32 @@ async def _():
     return models.Score(SCORE)
 
 
-@app.get("/boards")
-async def _(amount: int = 1):
-    if not 51 > amount > 0:
-        raise HTTPException(status_code=400, detail="Amount must be 51 > amount > 0!")
+@app.get("/board")
+async def _():
+    global OUTSTANDING_BOARDS
 
-    boards = [models.Board() for _ in range(amount)]
-    return boards
+    if len(OUTSTANDING_BOARDS) > BOARD_SIZE_LIMIT:
+        raise HTTPException(
+            status_code=400,
+            detail="Cannot provide another board until one is checked in!"
+        )
+
+    board = models.Board()
+    OUTSTANDING_BOARDS.append(board)
+    return board
 
 
 @app.post("/check")
-async def _():
+async def _(board: models.Board):
     global SCORE
-    SCORE += 1
+    global OUTSTANDING_BOARDS
+
+    correct_board = next((b for b in OUTSTANDING_BOARDS if b.id == board.id), None)
+    if not correct_board:
+        raise HTTPException(
+            status_code=400,
+            detail="Board not found!"
+        )
+
+    OUTSTANDING_BOARDS.remove(correct_board)
     return models.Score(SCORE)
